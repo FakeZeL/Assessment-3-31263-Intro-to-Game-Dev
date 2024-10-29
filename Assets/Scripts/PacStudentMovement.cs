f5054d0b9c00cec074955b6cc47d6eb1db93aff3
Tim <Timothy.Fu@student.uts.edu.au>
2024-10-29 16:52:57 +1100

    30%Z

--- Assets/Scripts/PacStudentMovement.cs
+++ Assets/Scripts/PacStudentMovement.cs
@@ -5,11 +5,16 @@ public class PacStudentController : MonoBehaviour
 {
     public GameObject pacStudent;
     public float moveSpeed = 2f;
+<<<<<<< Updated upstream
 
     private int currentWaypointIndex = 0;
     private bool isMoving = false;
 
     public Vector3[] waypoints;
+=======
+    private bool isMoving = false;
+
+>>>>>>> Stashed changes
 
     private Animator anim;
     private AudioSource walkingAudio;
@@ -21,6 +26,7 @@ public class PacStudentController : MonoBehaviour
         anim = pacStudent.GetComponent<Animator>();
         walkingAudio = pacStudent.GetComponent<AudioSource>();
 
+<<<<<<< Updated upstream
         waypoints = new Vector3[] {
             new Vector3(-7.5f, 3.5f, 0f),
             new Vector3(-7.5f, -0.5f, 0f),
@@ -29,16 +35,21 @@ public class PacStudentController : MonoBehaviour
         };
 
         targetPos = waypoints[currentWaypointIndex];
+=======
+>>>>>>> Stashed changes
     }
 
     void Update()
     {
+<<<<<<< Updated upstream
         if (Vector3.Distance(pacStudent.transform.position, targetPos) < 0.01f)
         {
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
             targetPos = waypoints[currentWaypointIndex]; // Update the target position
         }
 
+=======
+>>>>>>> Stashed changes
         MovePacStudent();
     }
 
@@ -63,8 +74,11 @@ public class PacStudentController : MonoBehaviour
             }
         }
 
+<<<<<<< Updated upstream
         pacStudent.transform.position = Vector3.MoveTowards(pacStudent.transform.position, targetPos, step);
 
+=======
+>>>>>>> Stashed changes
         if (Vector3.Distance(pacStudent.transform.position, targetPos) < 0.01f)
         {
             anim.SetBool("isMoving", false);
