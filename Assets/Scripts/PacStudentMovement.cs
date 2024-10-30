ec8f51996531ef0daee4248a6f82935d80b6d8cb
Tim <Timothy.Fu@student.uts.edu.au>
2024-10-30 14:20:42 +1100

    Update PacStudentMovement.cs

--- Assets/Scripts/PacStudentMovement.cs
+++ Assets/Scripts/PacStudentMovement.cs
@@ -5,20 +5,11 @@ public class PacStudentController : MonoBehaviour
 {
     public GameObject pacStudent;
     public float moveSpeed = 2f;
-<<<<<<< Updated upstream
-
-    private int currentWaypointIndex = 0;
     private bool isMoving = false;
-
-    public Vector3[] waypoints;
-=======
-    private bool isMoving = false;
-
->>>>>>> Stashed changes
+    private Vector3 targetPos;
 
     private Animator anim;
     private AudioSource walkingAudio;
-    private Vector3 targetPos;
 
     void Start()
     {
@@ -26,30 +17,10 @@ public class PacStudentController : MonoBehaviour
         anim = pacStudent.GetComponent<Animator>();
         walkingAudio = pacStudent.GetComponent<AudioSource>();
 
-<<<<<<< Updated upstream
-        waypoints = new Vector3[] {
-            new Vector3(-7.5f, 3.5f, 0f),
-            new Vector3(-7.5f, -0.5f, 0f),
-            new Vector3(-2.5f, -0.5f, 0f),
-            new Vector3(-2.5f, 3.5f, 0f)
-        };
-
-        targetPos = waypoints[currentWaypointIndex];
-=======
->>>>>>> Stashed changes
     }
 
     void Update()
     {
-<<<<<<< Updated upstream
-        if (Vector3.Distance(pacStudent.transform.position, targetPos) < 0.01f)
-        {
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
-            targetPos = waypoints[currentWaypointIndex]; // Update the target position
-        }
-
-=======
->>>>>>> Stashed changes
         MovePacStudent();
     }
 
@@ -74,11 +45,6 @@ public class PacStudentController : MonoBehaviour
             }
         }
 
-<<<<<<< Updated upstream
-        pacStudent.transform.position = Vector3.MoveTowards(pacStudent.transform.position, targetPos, step);
-
-=======
->>>>>>> Stashed changes
         if (Vector3.Distance(pacStudent.transform.position, targetPos) < 0.01f)
         {
             anim.SetBool("isMoving", false);
